cdcdb417cdd9681da3f258e64ffc50b9c48bc6ce
Vinigretic <v.bakatina@gmail.com>
2022-08-01 17:42:09 +0300

    Create endpoint Post - create model - create serializer - create views - create url

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    body = models.TextField(default='')
+    title = models.CharField(max_length=100, blank=True, default='')
+    owner = models.ForeignKey('auth.user', related_name='posts', on_delete=models.CASCADE)
+
+
+    class Meta:
+        ordering = ['created']
+
+
+
+
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from . import serializers
 from django.contrib.auth.models import User
+from .models import Post
 
 
 class UserList(generics.ListAPIView):
@@ -12,5 +13,17 @@ class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
 
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
 
 
